da973da8a71ad663a4ded4de3245aff68cfb017a
hiba-GA <hiba.formal@gmail.com>
2025-11-23 09:56:05 +0300

    update

---
+++ js/app.js
@@ -0,0 +1,101 @@
+/*-------------------------------- Constants --------------------------------*/
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5], 
+  [6, 7, 8], 
+  [0, 3, 6], 
+  [1, 4, 7], 
+  [2, 5, 8], 
+  [0, 4, 8], 
+  [2, 4, 6]
+]
+
+/*---------------------------- Variables (state) ----------------------------*/
+let board
+let turn
+let winner
+let tie
+
+/*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelectorAll(".sqr")
+const messageEl = document.getElementById("message")
+const resetBtnEl = document.getElementById("reset")
+
+/*-------------------------------- Functions --------------------------------*/
+function init() {
+  board = ["", "", "", "", "", "", "", "", ""]
+  turn = "X"
+  winner = false
+  tie = false
+  render()
+}
+
+function render() {
+  updateBoard()
+  updateMessage()
+}
+
+function updateBoard() {
+  board.forEach((cell, index) => {
+    squareEls[index].textContent = cell
+  });
+}
+
+function updateMessage() {
+  if (!winner && !tie) {
+    messageEl.textContent = `It's ${turn}'s turn`
+  } else if (!winner && tie) {
+    messageEl.textContent = "It's a tie!"
+  } else {
+    messageEl.textContent = `Congratulations ${turn} wins!`
+  }
+}
+
+function handleClick(event) {
+  const squareIndex = parseInt(event.target.id)
+
+  if (board[squareIndex] === "X" || board[squareIndex] === "O" || winner) {
+    return
+  }
+
+  placePiece(squareIndex)
+  checkForWinner()
+  checkForTie()
+  switchPlayerTurn()
+  render()
+}
+
+function placePiece(index) {
+  board[index] = turn
+}
+
+function checkForWinner() {
+  winningCombos.forEach(combo => {
+    const [a, b, c] = combo
+    if (board[a] !== "" && board[a] === board[b] && board[a] === board[c]) {
+      winner = true
+    }
+  })
+}
+
+function checkForTie() {
+  if (winner) return
+  if (!board.includes("")) {
+    tie = true
+  }
+}
+
+function switchPlayerTurn() {
+  if (winner) return
+  turn = turn === "X" ? "O" : "X"
+}
+
+/*----------------------------- Event Listeners -----------------------------*/
+squareEls.forEach(square => {
+  square.addEventListener("click", handleClick)
+})
+
+resetBtnEl.addEventListener("click", init)
+
+/*-------------------------------- Initialize -------------------------------*/
+init()
